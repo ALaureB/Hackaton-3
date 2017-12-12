3937ee301b27f3e06c84cdf45193e5d9062a206f
Anne-Laure Bienaimé <annelaure.bienaime@gmail.com>
2017-12-12 16:43:51 +0100

    commit

--- sushis_server/routes/index.js
+++ sushis_server/routes/index.js
@@ -24,14 +24,6 @@ router.all('*', function (req, res, next) {
         source: 'webhook'
       });
       break;
-    case 'prenom':
-      // Sur le site de sushi shop -> cheerio -> récupérer le prix 
-      console.log(req.body.result.parameters.product);
-      res.json({
-        speech: `${$given-name} quel joli prénom`,
-        source: 'webhook'
-      });
-      break;
 
     default:
       res.json({
