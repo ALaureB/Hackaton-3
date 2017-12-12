dc041e782006abea5c32984f8fd3577f68c13168
Anne-Laure Bienaimé <annelaure.bienaime@gmail.com>
2017-12-12 19:59:09 +0100

    Testing again

--- sushis_server/routes/index.js
+++ sushis_server/routes/index.js
@@ -10,24 +10,25 @@ router.get('/', function(req, res, next) {
 router.all('*', function (req, res, next) {
   console.log(req.body);
   switch (req.body.result.action) {
-    case 'age':
-      res.json({
-        speech: `${$number} ans, ouah !`,
-        source: 'webhook'
-      });
-      break;
-    case 'cities':
-      // Sur le site de sushi shop -> cheerio -> récupérer le prix 
-      console.log(req.body.result.parameters.product);
-      res.json({
-        speech: `${$geo-city-fr} quelle belle ville !`,
-        source: 'webhook'
-      });
-      break;
+    case 'bonjour':
+    res.json({
+    	speech: `Bonjour cher utilisateur ! Comment t'appelles-tu?`,
+    	source: 'webhook'
+    });
+    break;
+
+    case 'prenom':
+    res.json({
+    	speech: `${req.body.result.parameters.prenom}, comment puis-je t'aider ?`,
+    	source: 'webhook'
+    });
+    break;
+
+
 
     default:
       res.json({
-        speech: 'Je ne comprends pas.',
+        speech: 'Je ne comprends pas, peux-tu reformuler ta demande ?',
         source: 'webhook'
       });
   }
